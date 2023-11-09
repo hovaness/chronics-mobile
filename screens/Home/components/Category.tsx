38fb74c03a8b0eb4dc9b6dfec847c908f3829783
PRIZEOX <oganisyanararat1@gmail.com>
2023-11-09 20:11:37 +0400

    новые шрифты + подготовка компонента категорий

--- screens/Home/components/Category.tsx
+++ screens/Home/components/Category.tsx
@@ -1,16 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 
-type Props = {}
+type Props = {
+  name: string,
+  color: string,
+  desc: string,
+  img_code:string,
+}
 
 const Category = (props: Props) => {
   return (
     <View>
-      <Text>category</Text>
+      <Image source={{ uri:props.img_code }}/>
     </View>
   )
 }
 
-export default Category
+export default Category;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+})
--- screens/Home/screen.tsx
+++ screens/Home/screen.tsx
@@ -2,23 +2,25 @@ import { StyleSheet, Text, View } from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import React from 'react'
 import { useFonts } from 'expo-font'
+import Category from './components/Category'
 
 
 
 const HomeScreen = () => {
   const [fontsLoaded,error] = useFonts({
-    'Cormorant': require('../../assets/fonts/CormorantUnicase-Bold.ttf'),
-    'Cormorant-Light': require('../../assets/fonts/CormorantUnicase-SemiBold.ttf')
+   'Cormorant': require('../../assets/fonts/CormorantUnicase-Bold.ttf'),
+   'Jost-Light': require('../../assets/fonts/Jost-Light.ttf'),
+   'Jost':require('../../assets/fonts/Jost-Medium.ttf')
   })
-
 
   if(!fontsLoaded && !error){
     return null;
   }
 
   return (
-    <SafeAreaView>
-        <Text style= {{fontFamily:"Cormorant-Light", fontSize: 50}}>Хроники уебики</Text>
+    <SafeAreaView style = {styles.container}>
+      <Text style={styles.title}>Хроники</Text>
+      <Category name='Персонажи' img_code='' desc='Выучите всех персонажей из книг' color='#4293D4'/>
     </SafeAreaView>
   )
 }
@@ -26,5 +28,16 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-
+  container:{
+    marginTop:20,
+    marginHorizontal: 20,
+    flex: 1,
+  },
+  title: {
+    fontFamily: 'Cormorant',
+    fontSize: 32,
+    color: '#333',
+    fontWeight: "700",
+    textTransform:'uppercase',
+  }
 })
